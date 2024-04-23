c3ca50e9e8fdde139d5cd407ffcd1327f8572569
Xu Jiaqi <jiaqi.xu3@studio.unibo.it>
2024-04-23 15:31:33 +0200

    Created basic user-page

---
+++ front-end/script/user_page/user_page_client.js
@@ -0,0 +1,75 @@
+let backgrounds = ["grave","ruin","woods","swamp","pyro"];
+const backId = "background";
+const locId = "loc-name";
+function startGame() {
+    sendPage("./game_page.html");
+}
+
+function locForw() {    
+    let oldBack = "";
+    backgrounds.forEach(element => {
+        if(getDiv(backId).classList.contains(element)) {
+            oldBack = element;
+        }
+    });
+    let index = backgrounds.indexOf(oldBack)+1;
+    console.log(index);
+    
+    let newBack = index < backgrounds.length? index:0;
+    getDiv(backId).classList.remove(`${oldBack}`);
+    getDiv(backId).classList.add(`${backgrounds[newBack]}`);       
+    getDiv(locId).innerHTML = "The "+backgrounds[newBack];
+    //change location to explore
+}
+
+function locBack() {    
+    
+    let oldBack = "";
+    backgrounds.forEach(element => {
+        if(getDiv(backId).classList.contains(element)) {
+            oldBack = element;
+        }
+    });
+    let index = backgrounds.indexOf(oldBack)-1;
+    
+    let newBack = index > 0 ? index : backgrounds.length-1;
+    console.log(newBack);
+    getDiv(backId).classList.remove(`${oldBack}`);
+    getDiv(backId).classList.add(`${backgrounds[newBack]}`);       
+    getDiv(locId).innerHTML = "The "+backgrounds[newBack];
+    //change location to explore
+}
+
+function difForw() {
+    let difId = "dif-name";
+    let dif= getDiv(difId).innerHTML;
+    switch(dif) {
+        case "Normal":
+            getDiv(difId).innerHTML = "Hard";
+            break;
+        case "Easy":
+            getDiv(difId).innerHTML = "Normal";
+        break;
+        case "Hard":
+            getDiv(difId).innerHTML = "Easy";
+            break;
+    }
+    //do something
+}
+
+function difBack() {
+    let difId = "dif-name";
+    let dif= getDiv(difId).innerHTML;
+    switch(dif) {
+        case "Normal":
+            getDiv(difId).innerHTML = "Easy";
+            break;
+        case "Easy":
+            getDiv(difId).innerHTML = "Hard";
+        break;
+        case "Hard":
+            getDiv(difId).innerHTML = "Normal";
+            break;
+    }
+    //do something
+}
